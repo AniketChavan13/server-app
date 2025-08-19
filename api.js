0fdcea9439cb35303660828bd23403f4bdb427f7
Your Name <you@example.com>
2025-08-20 00:39:25 +0530

    Initial commit

--- api.js
+++ api.js
@@ -1,197 +1,102 @@
-var mongoClient = require("mongodb").MongoClient;
-var express = require("express");
-var cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 
-var conStr = " mongodb+srv://aniket:<98329832>@cluster0.qfnxera.mongodb.net/";
-
-var app = express();
-
+const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const conStr = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 
+// MongoDB connect helper
+async function getDatabase() {
+    const client = await MongoClient.connect(conStr);
+    return client.db("videolibrary");
+}
 
-app.get("/admin", (req, res) => {
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-        database.collection("admin").find({}).toArray().then(documents => {
-
-            res.send(documents);
-            res.end();
-        });
-    });
+// ROUTES
+app.get("/admin", async(req, res) => {
+    const db = await getDatabase();
+    const docs = await db.collection("admin").find({}).toArray();
+    res.send(docs);
 });
 
-app.get("/videos", (req, res) => {
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-
-
-        var database = clientObj.db("videolibrary");
-        database.collection("videos").find({}).toArray().then(documents => {
-
-            res.send(documents);
-            res.end();
-        });
-    });
-
+app.get("/videos", async(req, res) => {
+    const db = await getDatabase();
+    const docs = await db.collection("videos").find({}).toArray();
+    res.send(docs);
 });
 
-app.get("/categories", (req, res) => {
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-        database.collection("categories").find({}).toArray().then(documents => {
-
-            res.send(documents);
-            res.end();
-        });
-    });
+app.get("/categories", async(req, res) => {
+    const db = await getDatabase();
+    const docs = await db.collection("categories").find({}).toArray();
+    res.send(docs);
 });
 
-app.get("/users", (req, res) => {
-    mongoClient.connect(conStr).then(clientObj => {
-        const database = clientObj.db("videolibrary");
-        database.collection("users")
-            .find({})
-            .toArray()
-            .then(documents => {
-                res.send(documents);
-                res.end();
-            });
-    });
+app.get("/users", async(req, res) => {
+    const db = await getDatabase();
+    const docs = await db.collection("users").find({}).toArray();
+    res.send(docs);
 });
 
-app.get("/users/:id", (req, res) => {
-    mongoClient.connect(conStr).then(clientObj => {
-        const database = clientObj.db("videolibrary");
-        database.collection("users")
-            .findOne({ UserId: req.params.id })
-            .then(document => {
-                if (document) {
-                    res.send(document);
-                } else {
-                    res.status(404).send({ error: "User not found" });
-                }
-                res.end();
-            });
-    });
+app.get("/users/:id", async(req, res) => {
+    const db = await getDatabase();
+    const doc = await db.collection("users").findOne({ UserId: req.params.id });
+    if (!doc) return res.status(404).send({ error: "User not found" });
+    res.send(doc);
 });
 
-
-
-app.get("/video/:id", (req, res) => {
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-        database.collection("videos").find({ VideoId: parseInt(req.params.id) }).toArray().then(documents => {
-            res.send(documents);
-            res.end();
-        });
-    });
-
+app.get("/video/:id", async(req, res) => {
+    const db = await getDatabase();
+    const docs = await db
+        .collection("videos")
+        .find({ VideoId: parseInt(req.params.id) })
+        .toArray();
+    res.send(docs);
 });
 
-app.get("/videos/:categoryid", (req, res) => {
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-        database.collection("videos").find({ categoryId: parseInt(req.params.categoryId) }).toArray().then(documents => {
-            res.send(documents);
-            res.end();
-        });
-    });
-
+app.get("/videos/:categoryid", async(req, res) => {
+    const db = await getDatabase();
+    const docs = await db
+        .collection("videos")
+        .find({ CategoryId: parseInt(req.params.categoryid) })
+        .toArray();
+    res.send(docs);
 });
 
-app.post("/register-user", (req, res) => {
-
-    var user = {
-        UserId: req.body.UserId,
-        UserName: req.body.UserName,
-        Password: req.body.Password,
-        Mobile: req.body.Mobile,
-        Email: req.body.Email
-    }
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-        database.collection("users").insertOne(user).then(() => {
-
-            console.log('User Register');
-            res.end();
-        });
-    });
+app.post("/register-user", async(req, res) => {
+    const user = req.body;
+    const db = await getDatabase();
+    await db.collection("users").insertOne(user);
+    res.send({ message: "User registered successfully" });
 });
 
-app.post("/add-video", (req, res) => {
-
-    var video = {
-        VideoId: parseInt(req.body.VideoId),
-        Title: req.body.Title,
-        Url: req.body.Url,
-        Likes: parseInt(req.body.Likes),
-        Dislikes: parseInt(req.body.Dislikes),
-        Views: parseInt(req.body.Views),
-        CategoryId: parseInt(req.body.CategoryId)
-    }
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-
-        database.collection("videos").insertOne(video).then(() => {
-
-            console.log('Video Added');
-            res.end();
-        });
-    });
+app.post("/add-video", async(req, res) => {
+    const video = req.body;
+    const db = await getDatabase();
+    await db.collection("videos").insertOne(video);
+    res.send({ message: "Video added successfully" });
 });
 
-app.put("/edit-video/:id", (req, res) => {
-
-    var video = {
-        VideoId: parseInt(req.body.VideoId),
-        Title: req.body.Title,
-        Url: req.body.Url,
-        Likes: parseInt(req.body.Likes),
-        Dislikes: parseInt(req.body.Dislikes),
-        Views: parseInt(req.body.Views),
-        CategoryId: parseInt(req.body.CategoryId)
-    }
-
-    mongoClient.connect(conStr).then(clientObj => {
-        var database = clientObj.db("videolibrary");
-
-        database.collection("videos").updateOne({ VideoId: parseInt(req.params.id) }, { $set: video }).then(() => {
-
-            console.log('Video Updated');
-            res.end();
-        });
-    });
+app.put("/edit-video/:id", async(req, res) => {
+    const video = req.body;
+    const db = await getDatabase();
+    await db
+        .collection("videos")
+        .updateOne({ VideoId: parseInt(req.params.id) }, { $set: video });
+    res.send({ message: "Video updated successfully" });
 });
 
-app.delete("/delete-video/:id", (req, res) => {
-
-    mongoClient.connect(conStr).then(clientObj => {
-
-        var database = clientObj.db("videolibrary");
-
-        database.collection("videos").deleteOne({ VideoId: parseInt(req.params.id) }).then(() => {
-
-            console.log('Video Deleted');
-            res.end();
-        });
-    });
+app.delete("/delete-video/:id", async(req, res) => {
+    const db = await getDatabase();
+    await db
+        .collection("videos")
+        .deleteOne({ VideoId: parseInt(req.params.id) });
+    res.send({ message: "Video deleted successfully" });
 });
 
-app.listen(5000);
-console.log(`Server Started http://127.0.0.1:5000`);
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
